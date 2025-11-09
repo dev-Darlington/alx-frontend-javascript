e01d26af48e80057b82bbdbf7962c4bb51f52183
Darlington <darlingtonuchenna53@gmail.com>
2025-11-09 22:04:42 +0100

    updated main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -25,3 +25,24 @@ function printTeacher(firstName:string, lastName:string): string {
 interface printTeacherFunction {
     (firstName: string, lastName: string): string;
 }
+
+interface StudentClass {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+class StudentClass implements StudentClass {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+    displayName(): string {
+        return this.firstName;
+    }
+}
