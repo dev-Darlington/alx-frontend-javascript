fe17bbc143588f0cfe953e2f16fad7e887ccace8
Darlington <darlingtonuchenna53@gmail.com>
2025-11-12 12:46:24 +0100

    updated files

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,4 +1,4 @@
 import { RowID, RowElement } from "./interface";
 declare function insertRow(row: RowElement): number;
-declare function deleteRow(rowID: RowID): void;
+declare function deleteRow(rowId: RowID): void;
 declare function updateRow(rowID: RowID, rowElement: RowElement);
